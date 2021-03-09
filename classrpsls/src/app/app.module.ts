784d617457e5f41f2d6f88347606eb444e8c1b11
JohnCodePass <jmoralesc0259@gmail.com>
2021-03-09 10:54:07 -0800

    Main page updates

--- classrpsls/src/app/app.module.ts
+++ classrpsls/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,

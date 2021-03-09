b0ccbbfd01fabe7ff8353bde27302fae935f90a7
Your NameJoseph <you@example.comjosephracca@gmail.com>
2021-03-09 11:41:11 -0800

    API call successful with Routing Team

--- classrpsls/src/app/services/data-service.service.ts
+++ classrpsls/src/app/services/data-service.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class DataServiceService {
 
-  constructor() { }
+export class DataService {
+
+  private url: string = "https://csa2020studentapi.azurewebsites.net/rpsls"
+
+  constructor(private http: HttpClient) { }
+
+  GetAPIChoice() {
+    this.http.get(this.url, {responseType: "text"}).subscribe(response => console.log(response))
+
+    //.get is the ngew .fetch
+    //.subscribe is the ngew .then
+
+    // return this.http.get(this.url).subscribe()
+  }
 }

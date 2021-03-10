84886f93a50176a654ba52368cedd2d9aa39d0fd
huegogh <huegogh42@gmail.com>
2021-03-10 09:37:10 -0800

    add routing

--- classrpsls/src/app/pages/game/game.component.ts
+++ classrpsls/src/app/pages/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private router: Router) { 
+    
+  }
   ngOnInit(): void {
   }
+  
+  goToResults() {
+    this.router.navigate(['results']);
+  };
+  goToMain() {
+    this.router.navigate(['']);
+  };
 
 }
